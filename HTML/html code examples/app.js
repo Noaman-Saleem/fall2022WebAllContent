2f764a82b903f0671060a5984afede13cd32cd43
noaman saleem <noaman.saleem@umy.edu.pk>
2022-10-09 18:56:07 +0500

    first commit

---
+++ HTML/html code examples/app.js
@@ -0,0 +1,13 @@
+
+// console.log("My name is "+ fname);
+// console.log("My age is "+ age);
+
+var fname = "Noaman";
+var age = 23;
+
+console.log(typeof age);
+
+
+
+// let name = "Noaman";
+// let age = 23;
